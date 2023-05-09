682b34aa594e3f2aca371bcb91cd264425afa647
Jesslyn <jesslyngwn@gmail.com>
2023-05-09 11:17:36 +0700

    Revert "tes"
    This reverts commit ba33acb1fb3e81cef7b03c80d10cc8d8e538c441.

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ def generatePaketID():
     global paketCounter
     paketCounter += 1
     return f'PAKET{str(paketCounter).zfill(3)}'
-    
 
 # Define the duration-to-priority mapping
 global priority
